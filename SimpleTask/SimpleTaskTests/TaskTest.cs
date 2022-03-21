e0e212840697c9bfdaa4fba774256e3fccded254
PaulinaW <76110024+paulina-iit@users.noreply.github.com>
2022-03-21 23:03:11 +0100

    Simple sample program
    Task 0: Simple sample program

---
+++ SimpleTask/SimpleTaskTests/TaskTest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleTask;
+
+namespace SimpleTaskTests
+{
+    [TestClass()]
+    public class TaskTest
+    {
+        [TestMethod()]
+        public void TestExceptionCase()
+        {
+            BankAccount bank = new BankAccount("Amanda", 0);
+            try
+            {
+                bank.Debit(3);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                StringAssert.Contains(e.Message, "amount <= 0 or amount > balance");
+                return;
+            }
+            catch (Exception e)
+            {
+                StringAssert.Contains(e.Message, "balance = 0");
+                return;
+            }
+            Assert.Fail("Failed exception");
+        }
+        public void TestCreditCase()
+        {
+            BankAccount bank = new BankAccount("Amanda", 3);
+            bank.Credit(3);
+            Assert.AreEqual(5, bank.Balance);
+        }
+
+        public void TestDebitCase()
+        {
+            BankAccount bank = new BankAccount("Amanda", 3);
+            bank.Debit(3);
+            Assert.AreEqual(0, bank.Balance);
+        }
+    }
+}
