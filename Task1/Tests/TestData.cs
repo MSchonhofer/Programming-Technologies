018e63c97fc1382100b58126ff5a0cc227e7ebca
MSchonhofer <101186731+MSchonhofer@users.noreply.github.com>
2022-04-11 23:21:20 +0200

    TestData.cs finished

--- Task1/Tests/TestData.cs
+++ Task1/Tests/TestData.cs
@@ -49,39 +49,33 @@ namespace Tests
             Assert.IsTrue(dataRepository.GetBook(catalog).Catalog.Books[0].BookID == 1);
         }
 
+       
         [TestMethod]
         public void ReaderTest()
         {
 
             Assert.IsTrue(dataRepository.GetAllReaders().ToList().Count == 3);
-            dataRepository.AddReader(new Reader(90, "test10", "test20"));
+            dataRepository.AddReader(new Reader(90, "Morgan", "Welsh"));
             Assert.IsTrue(dataRepository.GetAllReaders().ToList().Count == 4);
-            dataRepository.AddReader(new Reader(91, "test11", "test21"));
+            dataRepository.AddReader(new Reader(91, "Cole", "Miller"));
             Assert.IsTrue(dataRepository.GetAllReaders().ToList().Count == 5);
 
-            Assert.IsTrue(dataRepository.GetReader(91).Name.Equals("test11"));
-            Assert.IsTrue(dataRepository.GetReader(91).Surname.Equals("test21"));
-            Assert.IsTrue(dataRepository.GetReader(91).ReaderID == 91);
-
-            Catalog catalog = dataRepository.GetCatalog(0);
-            Assert.IsTrue(dataRepository.GetBook(catalog).Catalog.Author.Equals("Dostoevsky"));
-            Assert.IsTrue(dataRepository.GetBook(catalog).Catalog.Title.Equals("Crime and Punishnment"));
-            Assert.IsTrue(dataRepository.GetBook(catalog).Catalog.Books.Count == 4);
-            Assert.IsTrue(dataRepository.GetBook(catalog).Catalog.Books[0].BookID == 1);
-
+            Assert.IsTrue(dataRepository.GetReader(91).Name.Equals("Cole"));
+            Assert.IsTrue(dataRepository.GetReader(91).Surname.Equals("Miller"));
+            Assert.IsTrue(dataRepository.GetReader(91).ReaderID == 91)
 
             dataRepository.DeleteReader(91);
             Assert.IsTrue(dataRepository.GetAllReaders().ToList().Count == 4);
 
 
-            Reader reader = new Reader(99, "test99a", "test99b");
-            dataRepository.UpdateReader(90, reader);
+           Reader r = new(99, "Nick", "Jones");
+            dataRepository.UpdateReader(90, r);
 
-            Assert.IsTrue(dataRepository.GetReader(99).Name.Equals("test99a"));
-            Assert.IsTrue(dataRepository.GetReader(99).Surname.Equals("test99b"));
+            Assert.IsTrue(dataRepository.GetReader(99).Name.Equals("Nick"));
+            Assert.IsTrue(dataRepository.GetReader(99).Surname.Equals("Jones"));
             Assert.IsTrue(dataRepository.GetReader(99).ReaderID == 99);
             Assert.IsTrue(dataRepository.GetAllReaders().ToList().Count == 4);
-        }
+        } 
 
 
         [TestMethod]
