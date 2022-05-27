4d15a4400be2d8cf9ac910566ff6f6758cae16f6
PaulinaW <76110024+paulina-iit@users.noreply.github.com>
2022-05-27 19:13:51 +0200

    some changes in presentation

--- Task1/Presentation/ViewModel/ViewModelBase.cs
+++ Task1/Presentation/ViewModel/ViewModelBase.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Data.API;
+using Logic.API;
 
 namespace Presentation.ViewModel.MVVM
 {
@@ -15,5 +17,30 @@ namespace Presentation.ViewModel.MVVM
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+
+        #region bookModel
+        private IEnumerable<IBook> booksModel;
+        public IEnumerable<IBook> BooksModel
+        {
+            get { return booksModel; }
+            set { booksModel = value; }
+        }
+        private IBook selectedBook;
+        public IBook SelectedBook
+        {
+            get { return selectedBook; }
+            set 
+            { 
+                selectedBook = value; 
+                OnPropertyChanged(nameof(SelectedBook));
+            }
+        }
+        public void ReturnBook()
+        {
+            
+        }
+        #endregion
     }
 }
+
+    
