c2ea0042893b4cc1abd1f086e2896cbcbd400c42
PaulinaW <76110024+paulina-iit@users.noreply.github.com>
2022-05-27 16:30:40 +0200

    model

---
+++ Task2/Presentation/Model/Model.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logic.API;
+
+namespace Presentation.Model
+{
+    public class Model : IModel
+    {
+        private ILibrary library;
+
+        public Model(ILibrary library = default(ILibrary))
+        {
+            if(library == null)
+            {
+                this.library = ILibrary.CreateLogic();
+            } else
+            {
+                this.library = library;
+            }
+        }
+        public ILibrary Library { get { return library; } }
+
+    }
+}
