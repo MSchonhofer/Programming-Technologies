83dab219cf9f4c18eaa22bb246e8ad847560f5c8
MSchonhofer <101186731+MSchonhofer@users.noreply.github.com>
2022-06-13 18:05:41 +0200

    Update DataRepository.cs

--- Library/Data/Implementation/DataRepository.cs
+++ Library/Data/Implementation/DataRepository.cs
@@ -61,7 +61,6 @@ namespace Data.Impl
             return null;
         }
 
-
         public override ICatalog GetCatalog(int index)
         {
             if (index >= 0 && index < data.Catalogs.Count)
@@ -191,7 +190,7 @@ namespace Data.Impl
             }
         }
 
-        /* public override int ValidBookID //current valid book ID
+        /* public override int ValidBookID //current valid book ID?
          {
              get { return data.ValidBookID; }
          }
