b9911fd50f750bb814b86c6f99c843d4b2b449fe
PaulinaW <76110024+paulina-iit@users.noreply.github.com>
2022-06-13 22:42:39 +0200

    main window

--- Library/Presentation/View/MainWindow.xaml.cs
+++ Library/Presentation/View/MainWindow.xaml.cs
@@ -24,5 +24,25 @@ namespace Presentation
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MessageBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
