0ec110afbcea8dac6897aa98e855a949ec453a6f
PaulinaW <76110024+paulina-iit@users.noreply.github.com>
2022-06-28 14:02:35 +0200

    service finished

---
+++ Library/Service/Implementation/Action.cs
@@ -0,0 +1,23 @@
+﻿using Service.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Implementation
+{
+    internal class Action : IAction
+    {
+        public int ActionID { get; set; }
+        public int? CatalogID { get; set; }
+        public int? ReaderID { get; set; }
+
+        public Action(int actionID, int? catalogID, int? readerID)
+        {
+            ActionID = actionID;
+            CatalogID = catalogID;
+            ReaderID = readerID;
+        }
+    }
+}
--- Library/Service/Implementation/LibraryService.cs
+++ Library/Service/Implementation/LibraryService.cs
@@ -6,8 +6,6 @@ using System.Threading.Tasks;
 using Service.API;
 using Data.API;
 using System.Collections;
-using ICatalog = Data.API.ICatalog;
-using IReader = Data.API.IReader;
 
 namespace Service.Implementation
 {
@@ -20,79 +18,130 @@ namespace Service.Implementation
             this.DataRepository = dataRepository;
         }
 
+        public Service.API.ICatalog Transfer(Data.API.ICatalog catalog)
+        {
+            if (catalog == null)
+            {
+                return null;
+            }
+            return new Catalog(catalog.CatalogID, catalog.Author, catalog.Title);
+        }
+
+        public Service.API.IReader Transfer(Data.API.IReader reader)
+        {
+            if (reader == null)
+            {
+                return null; 
+            }
+            return new Reader(reader.ReaderID, reader.Name, reader.Surname);
+        }
+
+        public Service.API.IAction Transfer(Data.API.IAction action)
+        {
+            if (action == null)
+            {
+                return null;
+            }
+            return new Action(action.ActionID, action.CatalogID, action.ReaderID);
+        }
+
         public override void AddAction(int id, int cID, int rID)
         {
-            throw new NotImplementedException();
+            DataRepository.AddAction(id, cID, rID);
         }
 
         public override void AddCatalog(int id, string author, string title)
         {
-            throw new NotImplementedException();
+            DataRepository.AddCatalog(id, author, title);
         }
 
         public override void AddReader(int id, string name, string surname)
         {
-            throw new NotImplementedException();
+            DataRepository.AddReader(id, name, surname);
         }
 
         public override void DeleteCatalog(int id)
         {
-            throw new NotImplementedException();
+            DataRepository.DeleteCatalog(id);
         }
 
         public override void DeleteReader(int id)
         {
-            throw new NotImplementedException();
+            DataRepository.DeleteReader(id);
         }
 
-        public override IAction GetAction(int id)
+        public override API.IAction GetAction(int id)
         {
-            throw new NotImplementedException();
+            return Transfer(DataRepository.GetAction(id));
         }
 
-        public override IEnumerable<IAction> GetAllActions()
+        public override IEnumerable<API.IAction> GetAllActions()
         {
-            throw new NotImplementedException();
+            var actions = DataRepository.GetAllActions();
+            var listOfActions = new List<API.IAction>();
+            foreach (var action in actions)
+            {
+                listOfActions.Add(Transfer(action));
+            }
+            return listOfActions;
         }
 
         public override IEnumerable<API.ICatalog> GetAllCatalogs()
         {
-            throw new NotImplementedException();
+            var catalogs = DataRepository.GetAllCatalogs();
+            var listOfCatalogs = new List<API.ICatalog>();
+            foreach (var catalog in catalogs)
+            {
+                listOfCatalogs.Add(Transfer(catalog));
+            }
+            return listOfCatalogs;
         }
 
         public override IEnumerable<API.IReader> GetAllReaders()
         {
-            throw new NotImplementedException();
+            var readers = DataRepository.GetAllReaders();
+            var listOfReaders = new List<API.IReader>();
+            foreach (var reader in readers)
+            {
+                listOfReaders.Add(Transfer(reader));
+            }
+            return listOfReaders;
         }
 
         public override IEnumerable<API.ICatalog> GetCatalogByAuthor(string author)
         {
-            throw new NotImplementedException();
+            var booksByAuthor = DataRepository.GetCatalogByAuthor(author);
+            var listOfBooks = new List<API.ICatalog>();
+            foreach(var book in booksByAuthor)
+            {
+                listOfBooks.Add(Transfer(book));
+            }
+            return listOfBooks;
         }
 
         public override API.ICatalog GetCatalogByID(int id)
         {
-            throw new NotImplementedException();
+            return Transfer(DataRepository.GetCatalogByID(id));
         }
 
         public override API.ICatalog GetCatalogByTitle(string title)
         {
-            throw new NotImplementedException();
+            return Transfer(DataRepository.GetCatalogByTitle(title));
         }
 
         public override API.IReader GetReader(int id)
         {
-            throw new NotImplementedException();
+            return Transfer(DataRepository.GetReader(id));
         }
 
         public override void UpdateCatalog(int id, string author, string name)
         {
-            throw new NotImplementedException();
+            DataRepository.UpdateCatalog(id, author, name);
         }
 
         public override void UpdateReader(int id, string name, string surname)
         {
-            throw new NotImplementedException();
+            DataRepository.UpdateReader(id, name, surname);
         }
     }
 }
